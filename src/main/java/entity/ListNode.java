32641f0f0d7ea4761396f87ff7cb5372f2ac0184
freedoow <freedoow@qq.com>
2021-10-11 00:00:57 +0800

    add

---
+++ src/main/java/entity/ListNode.java
@@ -0,0 +1,31 @@
+package entity;
+
+/**
+ * @author whb
+ * @Description:
+ * @Date 2021-10-10
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
