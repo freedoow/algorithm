16c5e2ef8010d08b4b536b03244d1bc57276c60f
freedoow <freedoow@qq.com>
2021-11-22 23:36:14 +0800

    -链表的基础实现

---
+++ src/main/java/com/course/line/linkedlist/LinkedList.java
@@ -0,0 +1,45 @@
+package com.course.line.linkedlist;
+
+/**
+ * @author whb
+ */
+public class LinkedList {
+    private class Node<E> {
+        E e;
+        Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this.e = e;
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
