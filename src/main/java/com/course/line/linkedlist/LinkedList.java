5eadbac6ea51f1b6b217fd61f47d7deb8956c81c
freedoow <freedoow@qq.com>
2021-11-27 22:50:54 +0800

    - 添加头尾中节点

--- src/main/java/com/course/line/linkedlist/LinkedList.java
+++ src/main/java/com/course/line/linkedlist/LinkedList.java
@@ -48,8 +48,8 @@ public class LinkedList<E> {
     /**
      * 查询指定索引的节点的值
      *
-     * @param index
-     * @return
+     * @param index 索引
+     * @return E
      */
     public E get(int index) {
         if (index < 0 || index >= size) throw new IllegalArgumentException("get fail, index fail");
@@ -63,7 +63,7 @@ public class LinkedList<E> {
     /**
      * 查询第一个值
      *
-     * @return
+     * @return E
      */
     public E getFirst() {
         return get(0);
@@ -72,9 +72,70 @@ public class LinkedList<E> {
     /**
      * 查询最后一个值
      *
-     * @return
+     * @return E
      */
     public E getLast() {
         return get(size - 1);
     }
+
+    /**
+     * 修改指定索引的值
+     *
+     * @param index 索引
+     * @param e     修改的值
+     */
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) throw new IllegalArgumentException("get fail, index fail");
+        Node curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = head.next;
+        }
+        curr.e = e;
+    }
+
+    /**
+     * 添加头节点
+     *
+     * @param e 新增节点需要存储的数据
+     */
+    public void addFirst(E e) {
+//        Node addNode = new Node(e) ;
+//        addNode.next = head;
+//        head = addNode
+
+        head = new Node(e, head);
+        size++;
+    }
+
+    /**
+     * 添加尾节点
+     *
+     * @param e
+     */
+    public void addLast(E e) {
+        add(size - 1, e);
+    }
+
+    /**
+     * 添加中间节点
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+        if (index < 0 || index >= size) throw new IllegalArgumentException("get fail, index fail");
+        if (index == 0) addFirst(e);
+
+        Node prev = head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = head.next;
+        }
+//        Node addNode = new Node(e);
+//        addNode.next = prev.next;
+//        prev.next = addNode;
+        prev.next = new Node(e, prev.next);
+
+        size++;
+    }
+
 }
