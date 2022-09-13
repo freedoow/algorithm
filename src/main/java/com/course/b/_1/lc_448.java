d0c4737f2c6a2a81c36ca67adb659466e366cd0d
freedoow <freedoow@qq.com>
2022-09-13 23:22:42 +0800

    - 448

---
+++ src/main/java/com/course/b/_1/lc_448.java
@@ -0,0 +1,26 @@
+package com.course.b._1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author whb
+ * @Description:
+ * @Date 2022-09-13
+ */
+public class lc_448 {
+    public List<Integer> findDuplicates(int[] nums) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int index = (nums[i] - 1) % n;
+            nums[index] += n;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= n) res.add(nums[i + 1]);
+        }
+
+        return res;
+    }
+}
